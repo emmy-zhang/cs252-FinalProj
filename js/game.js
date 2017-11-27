60277e2d31d88aa2abe598de381d7d2cd875885b
emmy917917 <emmy.j.zhang@gmail.com>
2017-11-27 11:47:37 -0500

    Initial commit

---
+++ js/game.js
@@ -0,0 +1,46 @@
+
+/* Game namespace */
+var game = {
+
+    // an object where to store game information
+    data : {
+        // score
+        score : 0
+    },
+
+
+    // Run on page load.
+    "onload" : function () {
+        // Initialize the video.
+        if (!me.video.init(640, 480, {wrapper : "screen", scale : "auto", scaleMethod : "flex-width"})) {
+            alert("Your browser does not support HTML5 canvas.");
+            return;
+        }
+
+        // Initialize the audio.
+        me.audio.init("mp3,ogg");
+
+        // set and load all resources.
+        // (this will also automatically switch to the loading screen)
+        me.loader.preload(game.resources, this.loaded.bind(this));
+    },
+
+    // Run on game resources loaded.
+    "loaded" : function () {
+      // set the "Play/Ingame" Screen Object
+      me.state.set(me.state.PLAY, new game.PlayScreen());
+
+      // register our player entity in the object pool
+      me.pool.register("mainPlayer", game.PlayerEntity);
+      // me.pool.register("CoinEntity", game.CoinEntity);
+      me.pool.register("EnemyEntity", game.EnemyEntity);
+
+      // enable the keyboard
+      me.input.bindKey(me.input.KEY.LEFT,  "left");
+      me.input.bindKey(me.input.KEY.RIGHT, "right");
+      me.input.bindKey(me.input.KEY.UP,     "jump", true);
+
+      // start the game
+      me.state.change(me.state.PLAY);
+    }
+};
