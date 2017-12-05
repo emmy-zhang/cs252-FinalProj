527b41f9bda044a7327a2aae428f546c6bea2987
emmy917917 <emmy.j.zhang@gmail.com>
2017-12-04 19:13:36 -0500

    Add scoreboard
     - Create new fonts

--- js/entities/HUD.js
+++ js/entities/HUD.js
@@ -6,23 +6,23 @@ game.HUD = game.HUD || {};
 
 
 game.HUD.Container = me.Container.extend({
+  init: function () {
+    // call the constructor
+    this._super(me.Container, 'init');
 
-    init: function() {
-        // call the constructor
-        this._super(me.Container, 'init');
+    // persistent across level change
+    this.isPersistent = true;
 
-        // persistent across level change
-        this.isPersistent = true;
+    // make sure we use screen coordinates
+    this.floating = true;
 
-        // make sure we use screen coordinates
-        this.floating = true;
+    // give a name
+    this.name = "HUD";
 
-        // give a name
-        this.name = "HUD";
-
-        // add our child score object at the top left corner
-        this.addChild(new game.HUD.ScoreItem(5, 5));
-    }
+    // add our child score object at the top left corner
+    this.addChild(new game.HUD.ScoreItem('first', -860, -10, 'left'));
+    this.addChild(new game.HUD.ScoreItem('second', -10, -10, 'right'));
+  }
 });
 
 
@@ -30,37 +30,50 @@ game.HUD.Container = me.Container.extend({
  * a basic HUD item to display score
  */
 game.HUD.ScoreItem = me.Renderable.extend({
-    /**
-     * constructor
-     */
-    init: function(x, y) {
+  /**
+   * constructor
+   */
+  init: function (name, x, y, align) {
 
-        // call the parent constructor
-        // (size does not matter here)
-        this._super(me.Renderable, 'init', [x, y, 10, 10]);
+    // call the parent constructor
+    // (size does not matter here)
+    this._super(me.Renderable, 'init', [x, y, 10, 10]);
 
-        // local copy of the global score
-        this.score = -1;
-    },
-
-    /**
-     * update function
-     */
-    update: function () {
-        // we don't do anything fancy here, so just
-        // return true if the score has been updated
-        if (this.score !== game.data.score) {
-            this.score = game.data.score;
-            return true;
-        }
-        return false;
-    },
-
-    /**
-     * draw the score
-     */
-    draw: function (context) {
-        // draw it baby !
+    // create the font object
+    if (name == 'first') {
+      this.font = new me.BitmapFont(me.loader.getBinary('PressStartBlue'), me.loader.getImage('PressStartBlue'));
+    } else {
+      this.font = new me.BitmapFont(me.loader.getBinary('PressStartRed'), me.loader.getImage('PressStartRed'));
     }
 
+    // font alignment to right, bottom
+    this.font.textAlign = align;
+    this.font.textBaseline = "bottom";
+
+    // local copy of the global score
+    this.name = name;
+    this.score = -1;
+  },
+
+  /**
+   * update function
+   */
+  update: function () {
+    // we don't do anything fancy here, so just
+    // return true if the score has been updated
+    if (this.score !== game.data.scores[this.name]) {
+      this.score = game.data.scores[this.name];
+      return true;
+    }
+    return false;
+  },
+
+  /**
+   * draw the score
+   */
+  draw: function (renderer) {
+    // draw it baby!
+    this.font.draw(renderer, game.data.scores[this.name], me.game.viewport.width + this.pos.x, me.game.viewport.height + this.pos.y);
+    // this.font.draw(renderer, game.data.score, me.game.viewport.width + this.pos.x, me.game.viewport.height + this.pos.y);
+  }
 });
--- js/game.js
+++ js/game.js
@@ -1,50 +1,53 @@
 /* Game namespace */
 var game = {
-    // an object where to store game information
-    data: {
-        // score
-        score: 0
-    },
-
-    // Run on page load.
-    "onload": function () {
-        // Initialize the video.
-        // if (!me.video.init(640, 480, {wrapper : "screen", scale : "auto", scaleMethod : "flex-width"})) {
-        if (!me.video.init(640, 520, {wrapper : "screen", scale : "auto", scaleMethod : "flex-width"})) {
-            alert("Your browser does not support HTML5 canvas.");
-            return;
-        }
-
-        // Initialize the audio.
-        me.audio.init("mp3,ogg");
-
-        // set and load all resources.
-        // (this will also automatically switch to the loading screen)
-        me.loader.preload(game.resources, this.loaded.bind(this));
-    },
-
-    // Run on game resources loaded.
-    "loaded": function () {
-      // set the "Play/Ingame" Screen Object
-      me.state.set(me.state.PLAY, new game.PlayScreen());
-
-      // register our player entity in the object pool
-      me.pool.register("player1", game.PlayerEntity);
-      me.pool.register("player2", game.Player2Entity);
-      // me.pool.register("CoinEntity", game.CoinEntity);
-      // me.pool.register("EnemyEntity", game.EnemyEntity);
-
-      // enable the keyboard for player 1
-      me.input.bindKey(me.input.KEY.A, "left1");
-      me.input.bindKey(me.input.KEY.D, "right1");
-      me.input.bindKey(me.input.KEY.W, "jump1", true);
-
-      // enable the keyboard for player 2
-      me.input.bindKey(me.input.KEY.LEFT,  "left2");
-      me.input.bindKey(me.input.KEY.RIGHT, "right2");
-      me.input.bindKey(me.input.KEY.UP, "jump2", true);
-
-      // start the game
-      me.state.change(me.state.PLAY);
+  // an object where to store game information
+  data: {
+    // score
+    scores: {
+      first: 0,
+      second: 0
     }
+  },
+
+  // Run on page load.
+  "onload": function () {
+    // Initialize the video.
+    // if (!me.video.init(640, 480, {wrapper : "screen", scale : "auto", scaleMethod : "flex-width"})) {
+    if (!me.video.init(640, 480, { wrapper: "screen", scale: "auto", scaleMethod: "flex-width" })) {
+      alert("Your browser does not support HTML5 canvas.");
+      return;
+    }
+
+    // Initialize the audio.
+    me.audio.init("mp3,ogg");
+
+    // set and load all resources.
+    // (this will also automatically switch to the loading screen)
+    me.loader.preload(game.resources, this.loaded.bind(this));
+  },
+
+  // Run on game resources loaded.
+  "loaded": function () {
+    // set the "Play/Ingame" Screen Object
+    me.state.set(me.state.PLAY, new game.PlayScreen());
+
+    // register our player entity in the object pool
+    me.pool.register("player1", game.PlayerEntity);
+    me.pool.register("player2", game.Player2Entity);
+    // me.pool.register("CoinEntity", game.CoinEntity);
+    // me.pool.register("EnemyEntity", game.EnemyEntity);
+
+    // enable the keyboard for player 1
+    me.input.bindKey(me.input.KEY.A, "left1");
+    me.input.bindKey(me.input.KEY.D, "right1");
+    me.input.bindKey(me.input.KEY.W, "jump1", true);
+
+    // enable the keyboard for player 2
+    me.input.bindKey(me.input.KEY.LEFT, "left2");
+    me.input.bindKey(me.input.KEY.RIGHT, "right2");
+    me.input.bindKey(me.input.KEY.UP, "jump2", true);
+
+    // start the game
+    me.state.change(me.state.PLAY);
+  }
 };
